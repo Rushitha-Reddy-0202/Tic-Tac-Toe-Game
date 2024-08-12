22b24f89dd0c011f49bfd40693a68ea3ab239640
Rushitha Reddy <rushithareddyponnella0202@gmail.com>
2024-08-12 12:52:42 +0530

    Add files via upload
    This is a simple Tic-Tac-Toe game project I created as a beginner in web development. The project was developed using HTML, CSS, and JavaScript.I learned a lot during this project, and I took help from various YouTube tutorials to understand the concepts and implementation. This project helped me gain practical experience in web development, and I'm excited to continue improving my skills.
    
    Feel free to check out the code and play the game!

---
+++ JS.js
@@ -0,0 +1,54 @@
+let play = document.querySelectorAll(".box");
+let reset = document.querySelectorAll(".reset");
+let newgame = document.querySelectorAll(".new");
+let msg1 = document.querySelectorAll(".msgs");
+let msg2 = document.querySelectorAll(".msg");
+const win=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+let player1=true;
+play.forEach((box) => {
+    box.addEventListener("click",() => {
+        if(player1)
+        {
+            box.innerText="X";
+            player1=false;
+        }
+        else
+        {
+            box.innerText="O";
+            player1=true;
+        }
+        box.disabled=true;
+        winner();
+    });
+}
+);
+const show = (winneris) => {
+    // msg2.innerText = `Congratulations, Winner is ${winneris}`;
+    // msg1.classList.remove("hide");
+    
+};
+const winner = () => {
+    for(let i of win)
+    {
+        let a = play[i[0]].innerText;
+        let b = play[i[1]].innerText;
+        let c = play[i[2]].innerText;
+        if(a!="" && b!="" && c!="")
+        {
+            if(a===b && b===c)
+            {
+                show(a);
+                return;
+            }
+        }
+    }
+};
